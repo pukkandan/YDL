677ba3c0a375ee20887f9b4eb0b124a5c6dc4757
pukkandan <pukkandan@gmail.com>
2021-12-12 08:31:09 +0530

    Revert "Add timeout to retry so that yt-dlp updater can run"
    This reverts commit 773a30ed9d97c93032f8abfadc2d96d4bcc28ae6.
    
    No longer needed in latest versions of yt-dlp

--- retry.cmd
+++ retry.cmd
@@ -8,7 +8,6 @@ if ["%WINVER%"]==["10"] call "terminal-format.cmd"
 set "retry_err=1"
 
 echo.
-echo.
 echo.%tf.y%^> %date% %time%%tf.0%
 echo.
 echo.RUNNING: %tf.c%%*%tf.0%
@@ -17,12 +16,10 @@ echo.
 call %* && set "retry_err=0"
 
 if ["%retry_err%"]==["1"] echo.%tf.y%%tf._xr%ERROR, Retrying...%tf._0%
-echo|set /p="%tf.b%---------------------------------------------------%tf.xy%"
+echo.%tf.b%---------------------------------------------------%tf.0%
+echo.
 
-if ["%retry_err%"]==["1"] (
-	timeout 10
-	goto :start
-)
+if ["%retry_err%"]==["1"] goto :start
 
 echo.%tf.b%DONE%tf.0%
 echo.
